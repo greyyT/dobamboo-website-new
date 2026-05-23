d9ea5cc8e4454534b660239d905999489dc1425f
greyy-nguyen <118145239+greyyT@users.noreply.github.com>
2026-05-24 00:17:25 +0700

    update

--- src/views/layouts/main-layout/footer.tsx
+++ src/views/layouts/main-layout/footer.tsx
@@ -1,38 +1,48 @@
 import Image from 'next/image';
+import { getTranslations } from 'next-intl/server';
 
 import { Route } from '@/constants/route';
 import { Link } from '@/i18n/navigation';
+import { getIntlFormat } from '@/utils/formatter';
 
-export default function Footer() {
+export default async function Footer() {
+  const t = await getTranslations('Default');
+
   return (
     <footer className="mt-10 pb-10 bg-main-bg pt-6 flex flex-col items-center px-4">
       <ul className="flex flex-col sm:grid sm:grid-cols-3 w-300 max-w-full lg:px-4 gap-y-4">
         <li>
-          <h4 className="uppercase text-[15px] text-subtitle leading-6 font-semibold tracking-[0.5px]">About</h4>
+          <h4 className="uppercase text-[15px] text-subtitle leading-6 font-semibold tracking-[0.5px]">
+            {t(getIntlFormat('footer', 'aboutSection'))}
+          </h4>
           <Link href={Route.ABOUT_US} className="text-sm text-[#0e1112] block">
-            About Us
+            {t(getIntlFormat('footer', 'aboutUs'))}
           </Link>
           <Link href={Route.OUR_STORY} className="text-sm text-[#0e1112] block">
-            Our Story
+            {t(getIntlFormat('footer', 'ourStory'))}
           </Link>
           <Link href={Route.MEET_THE_TEAM} className="text-sm text-[#0e1112] block">
-            Meet the Team
+            {t(getIntlFormat('footer', 'meetTheTeam'))}
           </Link>
         </li>
         <li>
-          <h4 className="uppercase text-[15px] text-subtitle leading-6 font-semibold tracking-[0.5px]">Contact</h4>
+          <h4 className="uppercase text-[15px] text-subtitle leading-6 font-semibold tracking-[0.5px]">
+            {t(getIntlFormat('footer', 'contactSection'))}
+          </h4>
           <Link href={Route.CAREER} className="text-sm text-[#0e1112] block">
-            Careers
+            {t(getIntlFormat('footer', 'careers'))}
           </Link>
           <Link href={Route.CONTACT} className="text-sm text-[#0e1112] block">
-            Contact us
+            {t(getIntlFormat('footer', 'contactUs'))}
           </Link>
           <Link href={Route.BLOG} className="text-sm text-[#0e1112] block">
-            Blog
+            {t(getIntlFormat('footer', 'blog'))}
           </Link>
         </li>
         <li>
-          <h4 className="uppercase text-[15px] text-subtitle leading-6 font-semibold tracking-[0.5px]">Follow us on</h4>
+          <h4 className="uppercase text-[15px] text-subtitle leading-6 font-semibold tracking-[0.5px]">
+            {t(getIntlFormat('footer', 'followUsOn'))}
+          </h4>
           <div className="flex gap-6 mt-5">
             <Link
               href="https://www.facebook.com/share/1CeQkbgWMM/?mibextid=wwXIfr"
@@ -40,7 +50,7 @@ export default function Footer() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image src="/images/facebook.png" alt="Facebook" width={30} height={30} />
+              <Image src="/images/facebook.png" alt={t(getIntlFormat('footer', 'facebook'))} width={30} height={30} />
             </Link>
             <Link
               href="https://www.pinterest.com/infothetrevita/"
@@ -48,7 +58,7 @@ export default function Footer() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image src="/images/pinterest.png" alt="Pinterest" width={30} height={30} />
+              <Image src="/images/pinterest.png" alt={t(getIntlFormat('footer', 'pinterest'))} width={30} height={30} />
             </Link>
             <Link
               href="https://www.instagram.com/the.trevita/?igsh=d2JwcDB1Nm5rNjh3&utm_source=qr"
@@ -56,7 +66,7 @@ export default function Footer() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image src="/images/instagram.png" alt="Instagram" width={30} height={30} />
+              <Image src="/images/instagram.png" alt={t(getIntlFormat('footer', 'instagram'))} width={30} height={30} />
             </Link>
 
             <Link
@@ -66,7 +76,7 @@ export default function Footer() {
               rel="noopener noreferrer"
             >
               <div className="w-7.5 h-7.5 bg-slate-600/80">
-                <Image src="/images/linkedin.png" alt="Instagram" width={30} height={30} />
+                <Image src="/images/linkedin.png" alt={t(getIntlFormat('footer', 'linkedin'))} width={30} height={30} />
               </div>
             </Link>
           </div>
